3d5341e379e827bc144e797af28e439a12eedd58
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-22 15:12:32 -0300

    implement database connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/RianNegreiros/clean-go-rest-api/internal/db"
+)
 
 // Run is the startup of the go application
 func Run() error {
 	fmt.Println("Starting the application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/RianNegreiros/clean-go-rest-api
 
 go 1.20
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.7
+)
