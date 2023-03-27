a0e9f1a8f839f9266e4d7ac036d9bd8a74183107
Rian Dos Santos <riannegreiros@gmail.com>
2023-03-27 19:04:48 -0300

    acceptance tests

---
+++ tests/health_test.go
@@ -0,0 +1,20 @@
+//go:build e2e
+// +build e2e
+
+package tests
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHealthCheckEndpoint(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().Get("http://localhost:8080/health")
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode())
+}
